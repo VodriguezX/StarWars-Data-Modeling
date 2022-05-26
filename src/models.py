6305471df66a403fd0db5d5cde3c6e21a116fe87
VodriguezX <vodriguezmergilles@live.com>
2022-05-26 05:26:51 +0000

    draft

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
-Base = declarative_base()
+ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = 'person'
@@ -28,6 +28,50 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+
+
+
+
+
+class StarWars (Base):
+    __tablename__ = 'starwars'
+
+    user = Column(String(250), nullable=False)
+    planet = Column(String(250), nullable=False)
+    charecter = Column(String(250), nullable=False)
+
+class User(Base):
+    __tablename__ = 'user'
+ 
+    user = Column(String(250), nullable=False)
+    favorties = Column(String(250))
+    starwars_user = Column(String(250), ForeignKey('starwars.user'))
+    person = relationship(StarWars)
+
+
+    class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    planet_name = Column(String(250), nullable=False)
+    population = 
+
+
+        class Charecter(Base):
+    __tablename__ = 'charecter'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    charecters = Column(String(250), nullable=False)
+    charecter_name = Column(String(250), nullable=False)
+    planet_found = Column(String(250), nullable=False)
+
+
+    def to_dict(self):
+        return {}
+
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
